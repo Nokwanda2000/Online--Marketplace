0b27414bdf738002eba84055986d025b7ecece4c
Nokwanda2000 <nokwandachonco262@gmail.com>
2024-11-06 11:38:03 +0200

    ran the build command

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Navigation from './components/navigation';
+import Navigation from './components/Navigation';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landingpage from './pages/Landingpage';
 import Footer from './components/Footer';
